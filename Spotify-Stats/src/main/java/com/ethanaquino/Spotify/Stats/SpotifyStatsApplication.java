865991335c571d0fbe90c8304cfef235604e3098
ethan.aquino <ethan.aquino.258@gmail.com>
2023-01-08 19:02:49 -0800

    initial commit

---
+++ Spotify-Stats/src/main/java/com/ethanaquino/Spotify/Stats/SpotifyStatsApplication.java
@@ -0,0 +1,13 @@
+package com.ethanaquino.Spotify.Stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyStatsApplication.class, args);
+	}
+
+}
